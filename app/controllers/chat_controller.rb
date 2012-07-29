9a831b112e9312896ab3cfdebccee3e53eda9847
Andrew Latham <acl68@case.edu>
2012-07-28 23:15:06 -0400

    People can write messages, and it saves them to the database.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,13 +1,32 @@
 class ChatController < ApplicationController
   require 'pusher'
   
+  @@usrid = -1
+  
   def index
+    ip = request.remote_ip
+    usr = User.where("ip = ?", ip)
+    if (usr.length > 0)
+      @@usrid = usr.first.id
+    else
+      usr = User.new
+      usr.ip = ip
+      usr.save
+      usr = User.where("ip = ?", ip)
+      @@usrid = usr.first.id
+    end
     render(:action => "index")
   end
   
   def new_message
-
-    Pusher['chatapp1'].trigger('new_message', {:message => 'Testing'})
+    if (params[:message])
+      msg = Message.new
+      msg.message = params[:message]
+      msg.user_id = @@usrid
+      msg.save
+        
+      Pusher['chatapp1'].trigger('new_message', {:message => params[:message]})
+    end
     
     respond_to do |format|
       format.js {
