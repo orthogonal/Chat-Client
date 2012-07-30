eeb377e7d79359b146d2f54290b9cbdad654982a
Andrew Latham <acl68@case.edu>
2012-07-29 23:17:54 -0400

    Keep messages within their own rooms

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -36,7 +36,7 @@ class ChatController < ApplicationController
         session[:username] = params[:username]
       end
         
-      Pusher['chatapp1'].trigger('new_message', {:message => params[:message].gsub("'","\\\\'"), :username => params[:username].gsub("'","\\\\'")})
+      Pusher['chatapp1'].trigger('new_message', {:message => params[:message].gsub("'","\\\\'"), :username => params[:username].gsub("'","\\\\'"), :roomid => session[:roomid]})
     end
     
     respond_to do |format|
